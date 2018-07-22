bb90f69f0bfca184da362c2a0ad1720261bff4f5
Andrew Good <andrewgood@Andrews-MBP.hitronhub.home>
2018-07-22 15:32:54 -0700

    initial commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,14 @@
 <template>
   <div class="home">
     <img src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome, Homie!"/>
+    <h1> Nice To have You. </h1>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from '../components/HelloWorld.vue'
 
 export default {
   name: 'home',
